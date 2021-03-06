d689ff761760cd955c47ad79050453775046da29
Dogan <doganmermertas@hotmail.de>
2021-03-06 12:55:04 +0100

    added renamer

--- setup/windows.ps1
+++ setup/windows.ps1
@@ -1,4 +1,4 @@
-git clone https://github.com/lagmoellertim/freenet-funk-api.git ./setup/resources
-pip3 install -r ./setup/resources/requirements.txt
 python -m pip install flask
 python -m pip install funkapi
+
+mv ./configuration/credentials_template.py ./configuration/credentials.py
